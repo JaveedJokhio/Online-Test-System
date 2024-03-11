163b76f8f8421284d212c248d0b75488a1cc93f1
Javeed <javeedjokhio48@gmail.com>
2024-03-11 10:15:15 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HomeComponent,FooterComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-angular-project';
+  
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+export const routes: Routes = [
+    {
+        path:'',
+        component: HomeComponent,
+        title:'Home Page'
+    },
+    {
+        path:'about',
+        component: AboutComponent,
+        title:'About Page'
+    },
+    {
+        path:'contact',
+        component: ContactComponent,
+        title:'Contact Page'
+    }
+];
